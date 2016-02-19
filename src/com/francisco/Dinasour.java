3299cd60237149d9a83a49224450cef38cc0cca9
Francisco <mt2167fo@go.minneapolis.edu>
2016-02-18 18:05:11 -0600

    practice

---
+++ src/com/francisco/Dinasour.java
@@ -0,0 +1,11 @@
+package com.francisco;
+
+/**
+ * Created by mt2167fo on 2/18/2016.
+ */
+public class Dinasour {
+    public void roar(){
+        System.out.println("GRRRR!!!!");
+    }
+
+}
---
+++ src/com/francisco/DinosaurSupervisor.java
@@ -0,0 +1,12 @@
+package com.francisco;
+
+public class DinosaurSupervisor {
+
+    public static void main(String[] args) {
+	Dinasour t_rex = new Dinasour();
+        Dinasour velociraptor = new Dinasour();
+
+        t_rex.roar();
+        velociraptor.roar();
+    }
+}
